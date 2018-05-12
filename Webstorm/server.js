12bcc33b573765fe097d08453e921858e7d9f92a
Kevin van Loon <34369608+KevinvanLoon@users.noreply.github.com>
2018-05-12 20:41:45 +0200

    test voor heroku

--- Webstorm/server.js
+++ Webstorm/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+
 const app = express();
 
-app.use('/apiv1', require('./routes/apiv1'));
-app.all('*', (req,respons)=>{
-    respons.status(500);
-    respons.json('invalid request');
+app.get('/api/hello', (request, res, next) =>{
+    console.log(request.url);
+    res.status(500);
+    res.json("Hello");
+    console.log("gelukt");
+    next();
 });
 
-app.listen(8080, ()=>{
-    console.log('server started');
+app.use('/apiv3', require('./routes/apiv3'));
+app.all('*', (request, respons)=>{
+    respons.status(500);
+});
+//
+app.listen(process.env.PORT, () =>{
+    console.log("the magic happens at !" +
+        "8080");
 });
