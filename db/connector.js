6d7164922f3ee9c92f26b2ab5bc7c5286006d8bf
KevinvanLoon <kloon3@avans.nl>
2018-05-14 14:41:22 +0200

    connector

---
+++ db/connector.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const config = require('../config');
+
+let db = mysql.createConnection( {
+    host: '188.166.109.108',
+    user: 'studentenhuis_user',
+    password: 'secret',
+    database: 'studentenhuis',
+    insecureAuth : true
+});
+
+console.log(db.host);
+
+db.connect( (error) => {
+    if(error) {
+        console.log(error);
+        return;
+    } else {
+        console.log("Connected to " + config.dbServer + ':' + config.dbSchema);
+    }
+});
+
+module.exports = db;
