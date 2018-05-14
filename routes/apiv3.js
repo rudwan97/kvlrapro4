285e73de4f56d707f22c62e80ebd7e53165b2d7e
Kevin van Loon <34369608+KevinvanLoon@users.noreply.github.com>
2018-05-14 14:29:57 +0200

    Revert "bezig met auth"
    This reverts commit a9210532c7842444f69f4ea66372380c5a7bccd7.

--- routes/apiv3.js
+++ routes/apiv3.js
@@ -1,58 +1,7 @@
 const express = require('express');
 const data = require('../modules/intel');
 const router = express.Router();
-const auth =  require('../auth/authentication');
-const users = require('../modules/users');
 
-//Zorgt ervoor dat je altijd moet inloggen
-router.all( new RegExp("[^(\/login)]"), function (req, res, next) {
-
-    //
-    console.log("VALIDATE TOKEN")
-
-    var token = (req.header('X-Access-Token')) || '';
-
-    auth.decodeToken(token, (err, payload) => {
-        if (err) {
-            console.log('Error handler: ' + err.message);
-            res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
-        } else {
-            next();
-        }
-    });
-});
-
-router.route('/login')
-
-    .post( function(req, res) {
-
-        //
-        // Get body params or ''
-        //
-        var username = req.body.username || '';
-        var password = req.body.password || '';
-
-        //
-        // Check in datasource for user & password combo.
-        //
-        //
-        result = users.filter(function (user) {
-            if( user.username === username && user.password === password) {
-                return ( user );
-            }
-        });
-
-        // Debug
-        console.log("result: " +  JSON.stringify(result[0]));
-
-        // Generate JWT
-        if( result[0] ) {
-            res.status(200).json({"token" : auth.encodeToken(username), "username" : username});
-        } else {
-            res.status(401).json({"error":"Invalid credentials, bye"})
-        }
-
-    });
 
 router.get('/cpu/intel/:year?', (req, res) =>{
 
