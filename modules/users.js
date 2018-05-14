a9210532c7842444f69f4ea66372380c5a7bccd7
Kevin van Loon <34369608+KevinvanLoon@users.noreply.github.com>
2018-05-14 14:18:56 +0200

    bezig met auth

---
+++ modules/users.js
@@ -0,0 +1,12 @@
+user = [
+    {
+        username: 'diederich',
+        password: '12345'
+    },
+    {
+        username: 'robin',
+        password: 'abcde'
+    }
+];
+
+module.exports = user;
