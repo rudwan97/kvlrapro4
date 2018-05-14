21d971c85aefbb6ed4a832a926f47fce55afcbc9
Rudwan <rudwanakhiat1997@gmail.com>
2018-05-14 15:56:17 +0200

    jwt test 2

--- .idea/auth/authentication.js
+++ .idea/auth/authentication.js
@@ -1,3 +1,4 @@
+
 const settings = require('../config.json');
 const moment = require('moment');
 const jwt = require('jwt-simple');
@@ -5,12 +6,11 @@ const jwt = require('jwt-simple');
 //
 // Encode (van username naar token)
 //
-function encodeToken(mail, id) {
+function encodeToken(username) {
     const playload = {
-        // exp: moment().add(10, 'days').unix(),
+        exp: moment().add(10, 'days').unix(),
         iat: moment().unix(),
-        sub: mail
-        sub: id
+        sub: username
     };
     return jwt.encode(playload, settings.secretkey);
 }
@@ -19,20 +19,17 @@ function encodeToken(mail, id) {
 // Decode (van token naar username)
 //
 function decodeToken(token, cb) {
-    const bearerHeader = req.headers['acces point']
 
     try {
-
-        if(typeof bearerHeader = )
         const payload = jwt.decode(token, settings.secretkey);
 
         // Check if the token has expired. To do: Trigger issue in db ..
-        // const now = moment().unix();
+        const now = moment().unix();
 
         // Check if the token has expired
-        // if (now > payload.exp) {
-        //     console.log('Token has expired.');
-        // }
+        if (now > payload.exp) {
+            console.log('Token has expired.');
+        }
 
         // Return
         cb(null, payload);
