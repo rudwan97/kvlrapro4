4aea5b9b4778b4c8521ac8f84cc8e62e2f1ffddc
KevinvanLoon <kloon3@avans.nl>
2018-05-14 15:57:14 +0200

    Alle get endpoints af behalve laatste
    Nog geen goede loop voor alle users

--- routes/apiv4.js
+++ routes/apiv4.js
@@ -7,7 +7,8 @@ router.get('/studentenhuis/:id?', (req,res,next) => {
 
     const id = req.params.id || '';
     if (id == '') {
-        db.query('SELECT * FROM studentenhuis',
+        db.query('SELECT * ' +
+            'FROM studentenhuis',
             (error, rows, fields) => {
                 if (error) {
                     res.status(500).json(error.toString())
@@ -16,7 +17,8 @@ router.get('/studentenhuis/:id?', (req,res,next) => {
                 }
             })
     } else {
-        db.query('SELECT * FROM `studentenhuis` WHERE `ID` = 1',
+        db.query('SELECT * ' +
+            'FROM `studentenhuis` WHERE `ID` = ' + id,
             (error, rows, fields) => {
                 if (error) {
                     res.status(500).json(error.toString())
@@ -26,5 +28,76 @@ router.get('/studentenhuis/:id?', (req,res,next) => {
             })
     }
 });
+
+router.get('/studentenhuis/:id/maaltijd/:maaltijd?', (req,res,next) => {
+    const meal = req.params.maaltijd || '';
+    const id = req.params.id || '';
+    if (meal == '') {
+        db.query('SELECT * ' +
+            'FROM `maaltijd` ' +
+            'WHERE `StudentenhuisID` = ' + id,
+            (error, rows, fields) => {
+                if (error) {
+                    res.status(500).json(error.toString())
+                } else {
+                    res.status(200).json(rows)
+                }
+            })
+    } else {
+        console.log('SELECT * ' +
+            'FROM `maaltijd` ' +
+            'WHERE `StudentenhuisID` = ' + id +
+             ' AND `ID` = '+ meal);
+        db.query('SELECT * FROM `maaltijd` ' +
+            'WHERE `StudentenhuisID` = ' + id +
+            ' AND `ID` = '+ meal,
+            (error, rows, fields) => {
+                if (error) {
+                    res.status(500).json(error.toString())
+                } else {
+                    res.status(200).json(rows)
+                }
+            })
+    }
+});
+
+router.get('/studentenhuis/:id/maaltijd/:maaltijd/deelnemers', (req,res,next) => {
+    const meal = req.params.maaltijd || '';
+    const id = req.params.id || '';
+    let users=[];
+    let userid=[];
+
+        db.query('SELECT UserID ' +
+            'FROM `deelnemers` ' +
+            'WHERE `StudentenhuisID` = ' + id +
+            ' AND `MaaltijdID` = '+ meal,
+            (error, rows, fields) => {
+                if (error) {
+                    res.status(500).json(error.toString())
+                } else {
+                    console.log(rows)
+                    users = rows;
+                    for (let i = 0; i < users.length; i++){
+                        console.log(users[i].UserID);
+                        userid[i] = users[i].UserID;
+                        console.log(userid)
+                    }
+                }
+            })
+
+
+        db.query('SELECT `Voornaam`, `Achternaam`, `Email` FROM `user` WHERE `ID` = ' + userid[0],
+        (error, rows, fields) => {
+            if (error) {
+                res.status(500).json(error.toString())
+            } else {
+                res.status(200).json(rows)
+                console.log(rows)
+            }
+        })
+
+});
+
+
 
 module.exports = router;
