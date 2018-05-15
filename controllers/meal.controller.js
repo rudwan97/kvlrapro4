197045948fbd63c8b9ec5f8d09ffccdf1577deb3
Kevin van Loon <34369608+KevinvanLoon@users.noreply.github.com>
2018-05-15 23:38:02 +0200

    Alle fouten afgehandeld en getest
    meal en studentenhuis werken volledig

--- controllers/meal.controller.js
+++ controllers/meal.controller.js
@@ -1,14 +1,7 @@
 const express = require('express');
-const users = require('../modules/user_ds');
-const router = express.Router();
 const db = require('../db/connector');
-const auth = require('../auth/authentication');
 const jwt = require('jwt-simple');
 const settings = require('../config');
-
-function callMeWhenDone(){
-    console.log("Async task completed")
-}
 
 function getid(req) {
     var token = (req.header('X-Access-Token')) || '';
@@ -16,6 +9,7 @@ function getid(req) {
     const id = payload.sub;
     return id;
 }
+
 module.exports = {
     addMeal(req, res) {
         console.log("////////////NEW REQUEST////////////");
@@ -31,85 +25,88 @@ module.exports = {
 
         const getHouse = 'SELECT * FROM studentenhuis WHERE `ID` = ' + houseId;
 
-        db.query(getHouse, (error, creatorResult) => {
-            console.log("Er is " + creatorResult.length + " huis gevonden met het meegegeven id")
-            if (creatorResult.length !== 0) {
-                if (creatorResult[0].ID == houseId) {
-                    console.log("Het huis met de meegegeven id is gevonden! ID =" + creatorResult[0].ID);
-                    db.query('INSERT INTO maaltijd (`Naam`, `Beschrijving`, `Ingredienten`, `Allergie`, `Prijs`, `UserID`, `StudentenhuisID`) VALUES (?,?,?,?,?,?,?)',
-                        [name, description, ingredients, allergy, price, requestId, houseId],
-                        (errorMeals, resultMeals) => {
-
-                            if (error) {
-                                console.log(error)
-                                res.status(500).json({"error": "An error occured while fetching the data"})
-                            }
-                            else {
-                                res.status(200).json({"message": "De maaltijd is succesvol toegevoegd!"});
-                                console.log("Actie succesvol")
-                            }
-                        })
+        if (name !== '' && description !== '' && ingredients !== '' && allergy !== '' && price !== '') {
+            db.query(getHouse, (error, houseResult) => {
+                //   console.log("Er is " + creatorResult.length + " huis gevonden met het meegegeven id")
+                if (houseResult.length !== 0) {
+                    if (houseResult[0].ID == houseId) {
+                        console.log("Het huis met de meegegeven id is gevonden! ID =" + houseResult[0].ID);
+                        db.query('INSERT INTO maaltijd (`Naam`, `Beschrijving`, `Ingredienten`, `Allergie`, `Prijs`, `UserID`, `StudentenhuisID`) VALUES (?,?,?,?,?,?,?)',
+                            [name, description, ingredients, allergy, price, requestId, houseId],
+                            (errorMeals, resultMeals) => {
+                                if (error) {
+                                    console.log(error)
+                                    res.status(500).json({"error": "Internal error"})
+                                }
+                                else {
+                                    res.status(200).json({"message": "De maaltijd is succesvol toegevoegd!"});
+                                    console.log("Actie succesvol")
+                                }
+                            })
+                    } else {
+                        res.status(404).json("Niet gevonden huisId bestaat niet)")
+                    }
                 } else {
-                    res.json("Bestaat niet")
+                    res.status(404).json({message: "Niet gevonden (huisId bestaat niet)"})
+                    console.log("Huis niet gevonden")
                 }
-            } else {
-                res.status(500).json({error: "Er bestaat geen huis met het meegegeven id"})
-                console.log("Huis niet gevonden")
-            }
-        })
+            })
+        }else{
+            res.status(412).json({"message": "Een of meer properties in de request body ontbreken of zijn foutief"})
+        }
     },
     deleteMeal(req, res) {
-        console.log("////////////NEW REQUEST////////////")
+        console.log("////////////NEW REQUEST////////////");
         console.log("ID: " + getid(req) + " is making a delete request!");
         var requestId = getid(req);
         var houseId = req.params.id;
         var mealId = req.params.mealid;
+        console.log("id uit endpoint van maaltijd is" + mealId);
 
 
         const getHouse = 'SELECT * FROM studentenhuis WHERE `ID` = ' + houseId;
-        const getCreatorOfMeal = 'SELECT * FROM maaltijd WHERE `UserID` =' + requestId;
-        const deleteQuery = 'DELETE FROM maaltijd WHERE `ID` = ' + mealId;
+        const getMeals = 'SELECT * FROM maaltijd WHERE `UserID` =' + requestId + ' AND `ID`=' + mealId;
+        const deleteQuery = 'DELETE FROM maaltijd WHERE `ID` = ' + mealId + ' AND `StudentenhuisID` = '  + houseId;
 
-        console.log(getCreatorOfMeal);
+        console.log(getHouse);
+        console.log(getMeals);
+        console.log(deleteQuery);
         db.query(getHouse, (error, houseResult) => {
-            console.log("Er is " + houseResult.length + " huis gevonden met het meegegeven huisid");
-            db.query(getCreatorOfMeal, (error, creatorResult) => {
-                console.log("aanmaker id:" + creatorResult[0].UserID);
-                console.log(houseResult.length !== 0)
-                if (houseResult.length !== 0 && creatorResult !== 0) {
-                    console.log(houseResult[0].ID == houseId);
+            db.query(getMeals, (error, mealsResult) => {
+                if (houseResult.length !== 0 && mealsResult.length !== 0) {
                     if (houseResult[0].ID == houseId) {
-                        console.log(creatorResult[0].UserID === requestId)
-                        console.log(creatorResult[0].ID === mealId);
-                        console.log(mealId);
-                        console.log(creatorResult[0].ID)
-                        if (creatorResult[0].UserID === requestId) {
-                            db.query(deleteQuery, (error, result) => {
-                                if (error) {
-                                    console.log(error)
-                                    res.status(500).json({"error": "An error occured while fetching the data"})
-                                }
-                                else {
-                                    res.status(200).json({"message": "De maaltijd is succesvol gedelete!"});
-                                    console.log("Actie succesvol")
-                                }
-                            })
+                        if (mealsResult[0].UserID === requestId) {
+                            if (mealsResult[0].ID == mealId) {
+
+                                db.query(deleteQuery, (error, result) => {
+                                    if (error) {
+                                        console.log(error)
+                                        res.status(500).json({"error": "Internal error"})
+                                    }
+                                    else {
+                                        res.status(200).json({"message": "De maaltijd is succesvol gedelete!"});
+                                        console.log("Actie succesvol")
+                                    }
+                                })
+                            }else {
+                                res.status(404).json({"error": "Niet gevonden (huisId of maaltijdId bestaat niet)"})
+                            }
                         } else {
-                            //TODO: APARTE ERROR MESSAGES.
-                            res.json("Deze bestaat niet of heb jij niet gemaakt!");
+                            res.status(409).json({"error": "Conflict (Gebruiker mag deze data niet verwijderen)"});
                         }
                     } else {
-                        console.log("iets niet gevonden")
+                        console.log({"error": "Niet gevonden (huisId of maaltijdId bestaat niet)"})
                     }
-                } else {
-                    res.status(500).json({error: "Er bestaat geen huis met het meegegeven id"})
-                    console.log("Huis niet gevonden")
+                }else {
+                    res.status(404).json({"error": "Niet gevonden (huisId of maaltijdId bestaat niet)"})
                 }
+
             })
         })
-
     },
     getMeal(req, res, next) {
+        console.log("////////////NEW REQUEST////////////");
+        console.log("ID: " + getid(req) + " is making a get request!");
         const meal = req.params.maaltijd || '';
         const id = req.params.id || '';
         if (meal == '') {
@@ -132,6 +129,8 @@ module.exports = {
                 ' AND `ID` = ' + meal, (error, rows, fields) => {
                 if (error) {
                     res.status(500).json(error.toString())
+                }else if(rows.length === 0){
+                    res.status(404).json({"message" : "Niet gevonden (huisId of maaltijdId bestaat niet)"})
                 } else {
                     res.status(200).json(rows)
                 }
@@ -152,40 +151,46 @@ module.exports = {
         var mealid = req.params.mealid;
 
 
-        const getHouse = 'SELECT * FROM studentenhuis WHERE `ID` = ' + houseId;
-        const getMeals = 'SELECT * FROM maaltijd WHERE `ID` = ' + mealid;
-        db.query(getHouse, (error, houseResult) => {
-            console.log("Er is " + houseResult.length + " huis gevonden met het meegegeven id");
-            db.query(getMeals, (error,mealsResult) => {
+        if (name !== '' && description !== '' && ingredients !== '' && allergy !== '' && price !== '') {
 
-                if (houseResult.length !== 0) {
-                    if (mealsResult.length !== 0) {
-                       if (requestId === mealsResult[0].UserID) {
-                           console.log("Het huis met de meegegeven id is gevonden! ID =" + houseResult[0].ID);
-                           db.query('UPDATE maaltijd SET `Naam` = ?, `Beschrijving` = ?, `Ingredienten` = ?, `Allergie` = ?, `Prijs` = ?, `UserID` = ?, `StudentenhuisID` = ?',
-                               [name, description, ingredients, allergy, price, requestId, houseId],
-                               (errorMeals, resultMeals) => {
 
-                                   if (errorMeals) {
-                                       console.log(errorMeals)
-                                       res.status(500).json({"error": "An error occured while fetching the data"})
-                                   }
-                                   else {
-                                       res.status(200).json({"message": "De maaltijd is succesvol upgedate!"});
-                                       console.log("Actie succesvol")
-                                   }
-                               })
-                       } else {
-                           res.json("Geen rechten")
-                       }
-                    }else{
-                        res.json("Geen maaltijd gevonden")
+            const getHouse = 'SELECT * FROM studentenhuis WHERE `ID` = ' + houseId;
+            const getMeals = 'SELECT * FROM maaltijd WHERE `ID` = ' + mealid;
+            db.query(getHouse, (error, houseResult) => {
+                console.log("Er is " + houseResult.length + " huis gevonden met het meegegeven id");
+                db.query(getMeals, (error, mealsResult) => {
+
+                    if (houseResult.length !== 0) {
+                        if (mealsResult.length !== 0) {
+                            if (requestId === mealsResult[0].UserID) {
+                                console.log("Het huis met de meegegeven id is gevonden! ID =" + houseResult[0].ID);
+                                db.query('UPDATE maaltijd SET `Naam` = ?, `Beschrijving` = ?, `Ingredienten` = ?, `Allergie` = ?, `Prijs` = ?, `UserID` = ?, `StudentenhuisID` = ?',
+                                    [name, description, ingredients, allergy, price, requestId, houseId],
+                                    (error, resultMeals) => {
+
+                                        if (error) {
+                                            console.log(error)
+                                            res.status(500).json({"error": "Internal error"})
+                                        }
+                                        else {
+                                            res.status(200).json({"message": "De maaltijd is succesvol upgedate!"});
+                                            console.log("Actie succesvol")
+                                        }
+                                    })
+                            } else {
+                                res.status(409).json({"message": "Conflict (Gebruiker mag deze data niet wijzigen)"})
+                            }
+                        } else {
+                            res.status(404).json({"message": "Niet gevonden (huisId of maaltijdId bestaat niet)"})
+                        }
+                    } else {
+                        res.status(404).json({message: "Niet gevonden (huisId of maaltijdId bestaat niet)"})
                     }
-                } else {
-                    res.status(500).json({error: "Er bestaat geen huis met het meegegeven id"})
-                    console.log("Huis niet gevonden")
-                }
+                })
             })
-        })
-    }
+        }else {
+            res.status(412).json({message: "Een of meer properties in de request body ontbreken of zijn foutief"})
+        }
+        }
+
 }
--- controllers/studentenhuis.controller.js
+++ controllers/studentenhuis.controller.js
@@ -15,6 +15,7 @@ function getid(req) {
 
 module.exports = {
     getHousesById(req, res, next) {
+
         console.log("ID: " + getid(req) + " is making a getHouse request!");
         const id = req.params.id || '';
 
@@ -36,6 +37,10 @@ module.exports = {
                 (error, rows, fields) => {
                     if (error) {
                         res.status(500).json(error.toString())
+                    } else if(rows.length === 0){
+                        res.status(404).json({
+                            "message": "Geen studentenhuis gevonden",
+                        })
                     } else {
                         res.status(200).json(rows)
                     }
@@ -48,20 +53,27 @@ module.exports = {
         var adress = req.body.adres || '';
         var id = getid(req) || '';
 
+
+
         const postquery = 'INSERT INTO `studentenhuis` (`Naam`,`Adres`,`UserID`)\n' +
             'VALUES(\'' + name + '\',\'' + adress + '\',' + id + ')'
-        console.log(postquery);
+     //   console.log(postquery);
+        if (name !== '' || adress !== '') {
 
-        db.query(postquery,
-            (error, rows, fields) => {
-                if (error) {
-                    res.status(500).json(error.toString())
-                } else {
-                    res.json({
-                        message: 'Studentenhuis toegevoegd.'
-                    })
-                }
-            })
+            db.query(postquery,
+                (error, rows, fields) => {
+                    if (error) {
+                        res.status(500).json(error.toString())
+                    } else {
+                        res.status(200).json({
+                            message: 'Studentenhuis toegevoegd.'
+                        })
+                    }
+                })
+        }else {
+            res.status(412).json({ "message": 'Een of meer properties in de request body ontbreken of zijn foutief'})
+            console.log("Een of meer properties in de request body ontbreken of zijn foutief")
+        }
     },
     updateHouse(req, res) {
         console.log("ID: " + getid(req) + " is making a update request!");
@@ -71,16 +83,22 @@ module.exports = {
         var id = getid(req) || '';
         var olduserid;
 
+        if (name === '' || adress === ''){
+
+        }
+
         try {
             const useridquery = 'SELECT * ' +
                 'FROM studentenhuis WHERE ID=' + houseId;
             console.log(useridquery)
             db.query(useridquery,
                 (error, rows, fields) => {
+                console.log(rows)
                     if (error) {
                         res.status(500).json(error.toString())
-                    } if (rows.size === 0) {
-                        console.log("huis niet gevonden")
+                    }else if (rows.length === 0) {
+                        res.status(404).json({message : "Huis niet gevonden"});
+                        console.log("House not found")
 
                     } else {
                         console.log(olduserid = rows[0].UserID);
@@ -89,22 +107,27 @@ module.exports = {
 
                         const updatequery = 'UPDATE studentenhuis SET `Naam` = \'' + name + '\', `Adres` = \'' + adress + '\' WHERE `UserID`= ' + id + ' AND `ID` = ' + houseId + '';
 
-                        console.log(updatequery);
-                        db.query(updatequery,
-                            (error, rows, fields) => {
-                                if (error) {
-                                    res.status(500).json(error.toString())
-                                } else if (olduserid !== id) {
-                                    res.json({
-                                        message: "niet geautoriseerd."
-                                    })
-                                } else {
-                                    res.json({
-                                        message: 'Studentenhuis geupdate.'
-                                    })
-                                }
+                        if (name !== '' && adress !== '') {
+                            console.log(updatequery);
+                            db.query(updatequery,
+                                (error, rows, fields) => {
+                                    if (error) {
+                                        res.status(500).json(error.toString())
+                                    } else if (olduserid !== id) {
+                                        res.status(409).json({
+                                            message: "Conflict (Gebruiker mag deze data niet wijzigen)"
+                                        })
+                                    } else {
+                                        res.status(200).json({
+                                            message: 'Studentenhuis geupdate.'
+                                        })
+                                    }
 
-                            })
+                                })
+                        }else {
+                            res.status(412).json({message:
+                                'Een of meer properties in de request body ontbreken of zijn foutief'})
+                        }
                     }
                 })
         } catch (e) {
@@ -126,8 +149,8 @@ module.exports = {
                     if (error) {
                         res.status(500).json(error.toString())
                     } else if (rows.length === 0 || rows.size === 0) {
-                        res.json({
-                            message: "Er is geen studentenhuis met het meegegeven id gevonden!"
+                        res.status(404).json({
+                            message: "Niet gevonden (huisId bestaat niet)"
                         })
                         houseFound = false;
                     } else {
@@ -142,11 +165,11 @@ module.exports = {
                                 if (error) {
                                     res.status(500).json(error.toString())
                                 } else if (idFromCreator !== idFromToken) {
-                                    res.json({
-                                        message: "niet geautoriseerd."
+                                    res.status(409).json({
+                                        message: "Conflict (Gebruiker mag deze data niet verwijderen)"
                                     })
                                 } else {
-                                    res.json({
+                                    res.status(200).json({
                                         message: 'Studentenhuis gedelete.'
                                     })
                                 }
